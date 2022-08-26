83b615bd77bcb8e05206d5b70a95c97a17e87b37
nagibaba <babeknaghiyev@Babeks-MacBook-Pro.local>
2022-08-26 19:13:29 +0400

    Remove comment

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import useStyles from './styles';
 import {Text as RNText, TextProps} from 'react-native';
 
-// export type TVariant = 'h1-bold' | 'h1-medium' | 'h1-regular' | 'h2-bold' | 'h2-medium' | 'h2-regular' | 'h3-bold' | 'h3-medium' | 'h3-regular' | 'h1-bold' | 'h1-medium' | 'h1-regular' |
 interface ITextProps extends TextProps {
   variant: keyof ReturnType<typeof useStyles>;
 }
