168eeec165162a634881deaed933f27d8e22b81e
Babak Naghiyev <bnagiyev@gmail.com>
2022-08-26 18:57:44 +0400

    Update colors (#2)
    * Update colors
    
    * Grammar issues
    
    * Fix ts err
    
    * Fix name white to main
    
    Co-authored-by: nagibaba <babeknaghiyev@Babeks-MacBook-Pro.local>

--- src/components/Theme/defaultTheme.ts
+++ src/components/Theme/defaultTheme.ts
@@ -1,43 +1,81 @@
+const GRADIENT_DEGREE = {
+  '45': {useAngle: true, angle: 45, angleCenter: {x: 0.5, y: 0.5}},
+};
 export const defaultTheme = {
   //Will apply default font for ios and android(San Francisco on iOS and Roboto on Android)
   //Change this font in your project to apply changes on all components
   fontFamily: 'System',
+  primary: {
+    main: '#2400ff',
+    light: '#D1D3FD',
+    dark: '#17019C',
+    contrast: '#000000',
+  },
+  error: {
+    main: '#CC2418',
+    light: '#FBDCDA',
+    dark: '#B91D12',
+    contrast: '#000000',
+  },
+  success: {
+    main: '#088940',
+    light: '#B3DFCA',
+    dark: '#007133',
+    contrast: '#FFFFFF',
+  },
+  warning: {
+    main: '#FE9E34',
+    light: '#FFDDB3',
+    dark: '#EB621C',
+    contrast: '#000000',
+  },
+  grey: {
+    main: '#C7CCD3',
+    light: '#e4e7ec',
+    extraLight: '#EFF1F3',
+    dark: '#667985',
+  },
+  text: {
+    primary: '#000000',
+    secondary: '#344054',
+    disabled: '#D2D2D2',
+    hint: '#667085',
+  },
   background: {
-    main: '#FFF',
+    main: '#ffffff',
+    // TODO: remove the line below after the current component design is ready
     disabled: '#FCFCFD',
   },
+  gradient: {
+    main: {
+      start: '#375267',
+      end: '#9CB2C9',
+      degree: GRADIENT_DEGREE[45],
+    },
+    light: {
+      start: '#D1DDEB',
+      end: '#EEF3F8',
+      degree: GRADIENT_DEGREE[45],
+    },
+    dark: {
+      start: '#14212E',
+      end: '#375267',
+      degree: GRADIENT_DEGREE[45],
+    },
+  },
+
+  // TODO: remove all vars below after the current component design is ready
   default: {
     dark: '#667085',
     main: '#C7CCD3',
     light: '#E4E7EC',
     extraLight: '#EFF1F3',
   },
-  primary: {
-    main: '#003CB8',
-    light: '#E7EAF9',
-    dark: '#001D9A',
-    contrast: '#FFF',
-  },
+
   secondary: {
     main: '#58329A',
     light: '#EDE7F4',
     dark: '#3D2181',
     contrast: '#FFF',
-  },
-  success: {
-    main: '#6CA78B',
-    contrast: '#fff',
-  },
-  text: {
-    primary: '#101828',
-    secondary: '#344054',
-    disabled: '#D2D2D2',
-    hint: '#667085',
-  },
-  error: {
-    main: '#EC726B',
-    light: '#FBDCDA',
-    dark: '#D92C20',
-    contrast: '#000',
   },
 };
