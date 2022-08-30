5ecdf3c57fb3d570e7bc52196e734c7478b162a1
nagibaba <babeknaghiyev@Babeks-MacBook-Pro.local>
2022-08-30 16:20:11 +0400

    Fix eslint

--- src/storybook/preview/ThemeChooser/index.tsx
+++ src/storybook/preview/ThemeChooser/index.tsx
@@ -1,4 +1,3 @@
-/*eslint-disable */
 import * as React from 'react';
 import {ThemeType, useTheme} from '../../../components/Theme';
 import {ScrollView, View} from 'react-native';
@@ -25,37 +24,41 @@ const ThemePreview = ({theme}: {theme: ThemeType}) => {
     >
       {Object.entries(theme).map(t => {
         const [header, colors] = t;
-        if (['fontFamily', 'gradient'].includes(header)) return null;
+        if (
+          ['fontFamily', 'gradient', 'default', 'secondary'].includes(header)
+        ) {
+          return null;
+        }
         return (
           <>
-            <View style={{marginTop: 20}}>
-              <Text variant="h3-bold">{header}</Text>
-              {Object.entries(colors).map(c => (
-                <>
-                  <View style={{marginTop: 10}}>
-                    <Text variant="h4-medium" color={'grey'}>
-                      {c[0]}: {JSON.stringify(c[1])}
-                    </Text>
-                    <View
-                      style={{
-                        width: 30,
-                        height: 20,
-                        backgroundColor: c[1] as string,
-                        shadowColor: '#000',
-                        shadowOffset: {
-                          width: 0,
-                          height: 2,
-                        },
-                        shadowOpacity: 0.25,
-                        shadowRadius: 3.84,
+            <Text variant="h3-bold">{header}</Text>
+            {Object.entries(colors).map(c => (
+              <>
+                {/* eslint-disable-next-line react-native/no-inline-styles */}
+                <View style={{marginTop: 10}}>
+                  <Text variant="h4-medium" color={'grey'}>
+                    {c[0]}: {JSON.stringify(c[1])}
+                  </Text>
+                  <View
+                    // eslint-disable-next-line react-native/no-inline-styles
+                    style={{
+                      width: 30,
+                      height: 20,
+                      backgroundColor: c[1] as string,
+                      shadowColor: '#000',
+                      shadowOffset: {
+                        width: 0,
+                        height: 2,
+                      },
+                      shadowOpacity: 0.25,
+                      shadowRadius: 3.84,
 
-                        elevation: 5,
-                      }}
-                    />
-                  </View>
-                </>
-              ))}
-            </View>
+                      elevation: 5,
+                    }}
+                  />
+                </View>
+              </>
+            ))}
             <Divider />
           </>
         );
@@ -65,4 +68,3 @@ const ThemePreview = ({theme}: {theme: ThemeType}) => {
 };
 
 export default ThemePreview;
-/*eslint-enable */
