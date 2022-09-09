b3a140842f58d058825e55154aca13e8f9a058f6
Alex Pilica <alexandru.pilica@further.ae>
2022-09-09 10:53:39 +0300

    HG-66

--- src/components/Elevation/styles.ts
+++ src/components/Elevation/styles.ts
@@ -15,24 +15,24 @@ export default createUseStyles(theme => ({
     shadowOffset: {width: 0, height: 2},
     shadowOpacity: 0.25,
     shadowRadius: 7,
-    elevation: 6,
+    elevation: 8,
   },
   medium: {
     shadowOffset: {width: 0, height: 4},
     shadowOpacity: 0.25,
     shadowRadius: 9,
-    elevation: 9,
+    elevation: 13,
   },
   heavy: {
     shadowOffset: {width: 0, height: 6},
     shadowOpacity: 0.35,
     shadowRadius: 13,
-    elevation: 12,
+    elevation: 18,
   },
   extraHeavy: {
     shadowOffset: {width: 0, height: 8},
     shadowOpacity: 0.4,
     shadowRadius: 16,
-    elevation: 16,
+    elevation: 23,
   },
 }));
