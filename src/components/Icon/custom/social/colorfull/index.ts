3976af8e0491fa3464aaa2b9baa0265c16cdeb70
Babak Naghiyev <bnagiyev@gmail.com>
2022-09-19 11:09:43 +0400

    Add new icons (#24)
    Co-authored-by: nagibaba <babeknaghiyev@Babeks-MacBook-Pro.local>

---
+++ src/components/Icon/custom/social/colorfull/index.ts
@@ -0,0 +1,12 @@
+export {default as apple} from './apple.svg';
+export {default as facebook} from './facebook.svg';
+export {default as google} from './google.svg';
+export {default as instagram} from './instagram.svg';
+export {default as linkedin} from './linkedin.svg';
+export {default as pinterest} from './pinterest.svg';
+export {default as signal} from './signal.svg';
+export {default as snapchat} from './snapchat.svg';
+export {default as telegram} from './telegram.svg';
+export {default as tiktok} from './tiktok.svg';
+export {default as twitter} from './twitter.svg';
+export {default as youtube} from './youtube.svg';
