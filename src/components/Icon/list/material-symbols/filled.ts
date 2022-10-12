ba975200f4ca2f75caf08d424b6126a1c35bc08f
vtovchenko <valeriy.tovchenko@gmail.com>
2022-10-12 14:25:15 +0300

    [GH-192] Fixed Unable to change or edit the label, placeholder text, button CTA text or anything using the control actions like in web version

--- src/components/Icon/list/material-symbols/filled.ts
+++ src/components/Icon/list/material-symbols/filled.ts
@@ -15,3 +15,4 @@ export { default as west } from '@material-symbols/svg-400/rounded/west-fill.svg
 export { default as east } from '@material-symbols/svg-400/rounded/east-fill.svg';
 export { default as visibility } from '@material-symbols/svg-400/rounded/visibility-fill.svg';
 export { default as visibility_off } from '@material-symbols/svg-400/rounded/visibility_off-fill.svg';
+export { default as credit_card } from '@material-symbols/svg-400/rounded/credit_card-fill.svg';
